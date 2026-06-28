ea4e8e9ed1ea8ccfb90585c5eeb2a9acddc4673c
shota-souka <shokoshokd@gmail.com>
2026-06-28 12:36:32 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,6 @@ self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("activate", event => {
+  self.registration.unregister();
 });
